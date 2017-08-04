0a4084fb712dfc20982d77ea05c5feef453402a7
dilwaria <dilwaria@gmail.com>
2017-08-04 17:06:37 +0530

    making modular templates

---
+++ resources/views/home/leftsidebar.blade.php
@@ -0,0 +1,121 @@
+<div class="col-md-3 col-sm-3 col-xs-12 hidden-xs">
+				 
+				     <div class="leftside left_sidebar">
+						<div class="latest-jobs">
+							<h1>LATEST ANNOUNCEMENTS</h1>
+							<div class="border"></div>
+							<div class="border1"></div>
+						</div>
+						<div class="job-content">
+							
+							<ul>
+								<li>
+									<h1>IT Department Manager</h1>
+								</li>
+								<li>
+									<h1>IT Department Manager</h1>
+								</li>
+								<li>
+									<h1>IT Department Manager</h1>
+								</li>
+								<li>
+									<h1>IT Department Manager</h1>
+								</li>
+								<li>
+									<h1>IT Department Manager</h1>
+								</li>
+								<li>
+									<h1>IT Department Manager</h1>
+								</li>
+							</ul>
+							<a style="padding-left: 25px;" href="">View all <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+						</div>
+						
+					</div>   
+					<div class="">
+						
+						<div class="panel-group wrap" id="accordion" role="tablist" aria-multiselectable="true">
+      <div class="panel">
+        <div class="panel-heading" role="tab" id="headingOne">
+          <h4 class="panel-title">
+        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Collapsible item #1
+        </a>
+      </h4>
+        </div>
+        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+          <div class="panel-body">
+           <ul class="list-unstyled">
+												<li>
+													<a href="#">State Govt Jobs 1</a>
+												</li>
+												<li>
+													<a href="#">State Govt Jobs 2</a>
+												</li>
+												<li>
+													<a href="#">State Govt Jobs 3</a>
+												</li>
+												<li>
+													<a href="#">State Govt Jobs 4</a>
+												</li>
+											</ul>
+          </div>
+        </div>
+      </div>
+      <!-- end of panel -->
+
+      <div class="panel">
+        <div class="panel-heading" role="tab" id="headingTwo">
+          <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Collapsible item #2
+        </a>
+      </h4>
+        </div>
+        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+          <div class="panel-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+          </div>
+        </div>
+      </div>
+      <!-- end of panel -->
+
+      <div class="panel">
+        <div class="panel-heading" role="tab" id="headingThree">
+          <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Collapsible item #3
+        </a>
+      </h4>
+        </div>
+        <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+          <div class="panel-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+          </div>
+        </div>
+      </div>
+      <!-- end of panel -->
+
+      <div class="panel">
+        <div class="panel-heading" role="tab" id="headingFour">
+          <h4 class="panel-title">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+          Collapsible item #4
+        </a>
+      </h4>
+        </div>
+        <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingFour">
+          <div class="panel-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch.
+          </div>
+        </div>
+      </div>
+      <!-- end of panel -->
+
+    </div>
+					</div>	
+					
+					<div class="career">
+						<img src="images/left-banner.jpg" title="left-banner" alt="left-banner" class="img-responsive">
+					</div>
+				</div>
---
+++ resources/views/home/rightsidebar.blade.php
@@ -0,0 +1,128 @@
+ <div class="col-md-3 col-sm-3 col-xs-12 hidden-xs" >
+					<div class="rightside">
+						<div class="border"></div>
+						<div class="search">
+						<input name="s" value="" class="form-control" placeholder="Search..." type="text">
+						<button type="button" class="btn" style="bottom:initial;"><i class="fa fa-search" aria-hidden="true"></i></button>
+					     </div>
+						<div class="latest-post">
+							<h1>LATEST JOBS</h1>
+							<div class="border"></div>
+							<div class="border1"></div>
+						</div>
+						<div class="job-content">
+							<img src="images/p1.jpg" alt="p1" title="p1">
+							<h1>IT Department Manager</h1>
+							<ul class="list-inline">
+								<li>
+									<a href="#"><i class="fa fa-bookmark" aria-hidden="true"></i> Full Time</a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i> Chandigarh</a>
+								</li>
+							</ul>
+							<a href="">Apply Now <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+						</div>
+						<div class="job-content">
+							<img src="images/p1.jpg" alt="p1" title="p1">
+							<h1>IT Department Manager</h1>
+							<ul class="list-inline">
+								<li>
+									<a href="#"><i class="fa fa-bookmark" aria-hidden="true"></i> Full Time</a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i> Chandigarh</a>
+								</li>
+							</ul>
+							<a href="">Apply Now <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+						</div>
+						<div class="job-content">
+							<img src="images/p1.jpg" alt="p1" title="p1">
+							<h1>IT Department Manager</h1>
+							<ul class="list-inline">
+								<li>
+									<a href="#"><i class="fa fa-bookmark" aria-hidden="true"></i> Full Time</a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i> Chandigarh</a>
+								</li>
+							</ul>
+							<a href="">Apply Now <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+						</div>
+						<div class="job-content">
+							<img src="images/p1.jpg" alt="p1" title="p1">
+							<h1>IT Department Manager</h1>
+							<ul class="list-inline">
+								<li>
+									<a href="#"><i class="fa fa-bookmark" aria-hidden="true"></i> Full Time</a>
+								</li>
+								<li>
+									<a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i> Chandigarh</a>
+								</li>
+							</ul>
+							<a href="">Apply Now <i class="fa fa-angle-double-right" aria-hidden="true"></i></a>
+						</div>
+						
+					</div>
+					
+					<div class="archive col-md-12 col-sm-12 col-xs-12">
+						<h1>LATEST RESULTS</h1>
+						<div class="border"></div>
+						<div class="border1"></div>
+						<ul class="list-unstyled">
+							<li>
+								<a href="#">
+									<p class="pull-left">Jan 2015</p>
+									<span class="pull-right">(25)</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<p class="pull-left">Feb 2015</p>
+									<span class="pull-right">(150)</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<p class="pull-left">Mar 2015</p>
+									<span class="pull-right">(256)</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<p class="pull-left">April 2015</p>
+									<span class="pull-right">(392)</span>
+								</a>
+							</li>
+							<li>
+								<a href="#">
+									<p class="pull-left">May 2015</p>
+									<span class="pull-right">(551)</span>
+								</a>
+							</li>
+						</ul>
+					</div>
+					
+					<div class="tag col-md-12 col-sm-12 col-xs-12">
+						<h1>TAGS</h1>
+						<div class="border"></div>
+						<div class="border1"></div>
+						<ul class="list-inline">
+							<li>
+								<a href="">Marketing</a>
+							</li>
+							<li>
+								<a href="">Designer</a>
+							</li>
+							<li>
+								<a href="">Jobs</a>
+							</li>
+							<li>
+								<a href="">Creative</a>
+							</li>
+							<li>
+								<a href="">Jobs</a>
+							</li>
+						</ul>
+					</div>
+				</div>
