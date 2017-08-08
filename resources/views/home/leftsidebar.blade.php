b4d9bd29da2ba44db126c8b650508490bdae4889
dilwaria <dilwaria@gmail.com>
2017-08-08 18:51:07 +0530

    left sidebar

--- resources/views/home/leftsidebar.blade.php
+++ resources/views/home/leftsidebar.blade.php
@@ -71,12 +71,12 @@
       <div class="panel">
         <div class="panel-heading" role="tab" id="headingOne">
           <h4 class="panel-title">
-        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
           StateList
         </a>
       </h4>
         </div>
-        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+        <div id="collapseTwo" class="panel-collapse collapse " role="tabpanel" aria-labelledby="headingOne">
           <div class="panel-body">
            <ul class="list-unstyled">
             @foreach($stateList as $list)
@@ -94,12 +94,12 @@
       <div class="panel">
         <div class="panel-heading" role="tab" id="headingTwo">
           <h4 class="panel-title">
-        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
           Education Qualification
         </a>
       </h4>
         </div>
-        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+        <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
           <div class="panel-body">
             <ul class="list-unstyled">
           
@@ -118,12 +118,12 @@
       <div class="panel">
         <div class="panel-heading" role="tab" id="headingThree">
           <h4 class="panel-title">
-        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
           Job Categories
         </a>
       </h4>
         </div>
-        <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+        <div id="collapseFour" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
           <div class="panel-body">
            <ul class="list-unstyled">
           
@@ -138,6 +138,8 @@
         </div>
       </div>
       <!-- end of panel -->
+
+
 
       
 
